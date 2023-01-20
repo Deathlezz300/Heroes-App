241006bcc537f40b965685dc9e79e2ffc770a597
Alejandro <jairoalejandro2003@gmail.com>
2023-01-20 09:27:42 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+
+export const App = () => {
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  )
+}
+
